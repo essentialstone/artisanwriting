fa9f57fdb21e75170a52d9021b4e6ac3a7ea695e
Jeremy Cloud <jeremy.cloud@pm.me>
2022-01-07 14:27:41 -0600

    added migrate to insert magic sentences

--- scripts/sql_generator_migrator.py
+++ scripts/sql_generator_migrator.py
@@ -5,13 +5,13 @@ file_pointer = './magic-flavor.txt'
 def sql_gen(line):
 	table = 'Sentence'
 	cols = '(words,sourceId)'
-	return f'''INSERT INTO "{table}" {cols} ('{line}', 1);\n'''
+	return f'''INSERT INTO "{table}" {cols} VALUES ('{line}', 1);\n'''
 
 def file_writer():
 	with open(file_pointer,'r+') as f:
 		lines = f.readlines()
 		print('...ingesting {} words'.format(len(lines)))
-		with open('gend_output_insert_sql.txt','w+') as w:
+		with open('./insert-magic-sentences.sql','w+') as w:
 			for line in lines:
 				w.write(sql_gen(line.strip('\n')))
 			w.close()
